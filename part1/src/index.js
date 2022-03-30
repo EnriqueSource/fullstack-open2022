1e10577ff2a81ad3e0c3a6a9d121a84618ba6b9e
EnriqueSource <enriqueml@blogdeenrique.com>
2022-03-30 22:00:19 +0200

    Course information:step 2 close #3

--- part1/src/index.js
+++ part1/src/index.js
@@ -14,11 +14,9 @@ const Content = (props) => {
   console.log('Content submitted');
   return (
     <div>
-      {props.part1}
-      <br />
-      {props.part2}
-      <br />
-      {props.part3}
+        <p>{props.part1} {props.exercises1}</p>
+        <p>{props.part2} {props.exercises2}</p>
+        <p>{props.part3} {props.exercises3}</p>
     </div>
   )
 }
@@ -44,7 +42,9 @@ const App = () => {
   return (
     <>
       <Header course={course} />
-      <Content part1={part1 + ':' + ' ' + exercises1} part2={part2 + ':' + ' ' + exercises2} part3={part3 + ':' + ' ' + exercises3} />
+      <Content part1={part1} exercises1={exercises1} />
+      <Content part2={part2} exercises2={exercises2} />
+      <Content part3={part3} exercises3={exercises3}/>
       <Total total={exercises1 + exercises2 + exercises3} />
     </>
   )
