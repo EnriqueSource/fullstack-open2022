50033e5b0dc359ecf23415651d22c86011b1e884
EnriqueSource <enriqueml@blogdeenrique.com>
2022-05-26 09:27:50 +0200

    anecdotes part2 close #16

--- part1/anecdote/src/index.js
+++ part1/anecdote/src/index.js
@@ -1,6 +1,14 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
+const VotesDisplay = (props) => {
+  return (
+    <div value={props.votes}>
+      {props.text} {props.value}
+    </div>
+  );
+};
+
 const Button = (props) => {
   return <button onClick={props.onClick}>{props.text}</button>;
 };
@@ -8,15 +16,49 @@ const Button = (props) => {
 const App = (props) => {
   const [selected, setSelected] = useState(0);
 
+  /*
+    State that stores the votes.
+    The value of the initial state is an array full of zeros
+    with the same length as the constant "anecdotes" (6).
+  */
+  const [points, setPoints] = useState(Array(6).fill(0));
+
+  const [votes, setVotes] = useState(0);
+
   // Event handlers
+  const handleVoteClick = () => {
+    const newPoints = [...points];
+    setPoints(newPoints);
+    newPoints[selected] += 1;
+    setVotes(newPoints[selected]);
+  };
   const handleNextClick = () => {
-    const randomAnecdotes = Math.floor(Math.random() * anecdotes.length);
-    setSelected(randomAnecdotes);
+    const newPoints = [...points];
+    setPoints(newPoints);
+    const selected = Math.floor(Math.random() * anecdotes.length);
+    setSelected(selected);
+    setVotes(newPoints[selected]);
   };
 
   return (
     <>
       <div>{props.anecdotes[selected]}</div>
+      <table>
+        <tbody>
+          <tr>
+            <td>
+              <VotesDisplay text="has" />
+            </td>
+            <td>
+              <VotesDisplay value={votes} />
+            </td>
+            <td>
+              <VotesDisplay text="votes" />
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <Button onClick={handleVoteClick} text="vote" />
       <Button onClick={handleNextClick} text="next anecdote" />
     </>
   );
