43a16e0aeb13176390917f49fc58e2a219262952
EnriqueSource <enriqueml@blogdeenrique.com>
2022-06-26 15:33:12 +0200

    Exercise 2.6- App The Phonebook: step 4, add search field by name close #31

--- part2/the-phonebook/src/App.js
+++ part2/the-phonebook/src/App.js
@@ -2,9 +2,15 @@ import React, { useState } from "react";
 import Person from "./components/Person";
 
 const App = () => {
-  const [persons, setPersons] = useState([]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [filterName, setFilterName] = useState("");
 
   // Event handler
   const addPerson = (event) => {
@@ -37,9 +43,24 @@ const App = () => {
     console.log(event.target.value);
   };
 
+  const handleFilterChange = (event) => {
+    setFilterName(event.target.value);
+    console.log(event.target.value);
+
+    const filter = new RegExp(filterName, "i");
+    const filteredName = () =>
+      persons.filter((person) => person.name.match(filter));
+    setPersons(filteredName);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with:{" "}
+        <input value={filterName} onChange={handleFilterChange} />
+      </div>
+      <h2>Add a new person</h2>{" "}
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
