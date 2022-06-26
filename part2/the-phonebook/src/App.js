bcac753e0a6850138f7df478b748cd29002f6ea5
EnriqueSource <enriqueml@blogdeenrique.com>
2022-06-26 10:48:41 +0200

    Exercise 2.6- App The Phonebook: step 2, add phone numbers close #29

--- part2/the-phonebook/src/App.js
+++ part2/the-phonebook/src/App.js
@@ -1,40 +1,51 @@
 import React, { useState } from "react";
 import Person from "./components/Person";
 
-const App = (props) => {
+const App = () => {
   const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   // Event handler
-  const addName = (event) => {
+  const addPerson = (event) => {
     event.preventDefault();
-    const person = persons.filter((person) => person.content === newName);
+    const person = persons.filter((person) => person.name === newName);
 
     if (person.length !== 0) {
       alert(`${newName} is already added to phonebook`);
     } else {
       const personObject = {
-        content: newName,
+        name: newName,
+        number: newNumber,
         id: persons.length + 1,
       };
       setPersons(persons.concat(personObject));
       setNewName("");
+      setNewNumber("");
 
       console.log(personObject);
     }
   };
 
-  const handlePersonsChange = (event) => {
+  const handleNameChange = (event) => {
     setNewName(event.target.value);
+    console.log(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
     console.log(event.target.value);
   };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addName}>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input value={newName} onChange={handlePersonsChange} />
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
@@ -48,7 +59,7 @@ const App = (props) => {
       </div>
       <p>
         <strong>debug: </strong>
-        {newName}
+        {newName} {newNumber}
       </p>
     </div>
   );
