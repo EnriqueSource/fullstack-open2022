79af4a6439e7d34c3927af055a35d14fa0694d2b
EnriqueSource <enriqueml@blogdeenrique.com>
2022-11-13 18:28:14 +0100

    Exercise 2.15 - the phone book app: save data in the backend server close #57

--- part2/the-phonebook/src/App.js
+++ part2/the-phonebook/src/App.js
@@ -30,13 +30,18 @@ const App = () => {
       const personObject = {
         name: newName,
         number: newNumber,
-        id: persons.length + 1,
       };
       setPersons(persons.concat(personObject));
       setNewName("");
       setNewNumber("");
 
       console.log(personObject);
+
+      axios
+        .post('http://localhost:3001/persons', personObject)
+        .then(response => {
+          setPersons(persons.concat(response.data));
+      })
     }
   };
 
